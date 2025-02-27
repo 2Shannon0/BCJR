3fba292ddf54477e6969f26a4d5ec1f4730ded80
i17m5 <ochenmoguchiy@yandex.ru>
2025-02-27 18:16:15 +0300

    исполняемый декодер, наглядная версия 5 картнок

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from BCJR import BCJRDecoder
+from BCJR_visible import BCJRDecoder
 from trellis4decoder import Trellis
 import numpy as np
 
@@ -7,14 +7,26 @@ t_h = Trellis('matricies/file_hamming.csv')
 
 t_h.build_trellis()
 # t_h.plot_sections()
-# print(t_h.vex)
+print('len(t_h.vex): ',len(t_h.vex),'\n')
+print('t_h.vex[0]): ',t_h.vex[0],'\n')
+print('t_h.vex[1]): ',t_h.vex[1],'\n')
+print('t_h.vex[2]): ',t_h.vex[2],'\n')
+
+
 # print(t_h.edg)
+
+# t_h2 = Trellis(vex=t_h.vex, edg=t_h.edg)
+
+# t_h2.plot_sections()
 
 H = np.array([[1, 1, 0, 1, 1, 0, 0],
               [1, 0, 1, 1, 0, 1, 0],
               [0, 1, 1, 1, 0, 0, 1]])
-    
+
 decoder = BCJRDecoder(t_h.vex, t_h.edg, H, sigma=0.5)
-received = np.array([1.2, -0.8, 0.5, -1.1, 0.9, -0.7, 0.3])  # Пример принятых значений
-decoded = decoder.decode(received)
-print("Декодированное слово:", decoded)
+# received = np.array([1.2, -0.8, 0.5, -1.1, 0.9, -0.7, 0.3])
+decoder.decode([1, 1.2, -0.5, 1, 1, 1, 0.5], 0.8)
+# print("Декодированное слово:", decoded)
+# received = np.array([1.2, -0.8, 0.5, -1.1, 0.9, -0.7, 0.3])  # Пример принятых значений
+# decoded = decoder.decode(received)
+# print("Декодированное слово:", decoded)
