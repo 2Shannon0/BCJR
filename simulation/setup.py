15a1c85312cd9bc94d380646d5fa2fdf1f408e71
alexey.a.bandyukov <alexey.a.bandyukov@megafon.ru>
2025-03-26 12:39:50 +0300

    improvements on trellis build

---
+++ simulation/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, Extension
+import pybind11
+
+ext_modules = [
+    Extension(
+        "bcjr_decoder",
+        ["bcjr_decoder.cpp"],
+        include_dirs=[pybind11.get_include()],
+        language="c++",
+        extra_compile_args=["-std=c++17"],  # Добавили поддержку C++17
+    ),
+]
+
+setup(
+    name="bcjr_decoder",
+    ext_modules=ext_modules,
+)
